dce8e45898338e1d91979518cff924a3569d35bc
seminire.adebiyi <seminire.adebiyi@kuda.com>
2022-08-31 15:03:50 +0100

    Add project files.

---
+++ RecipesManger/Controllers/RecipesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecipesManger.Models;
+
+namespace RecipesManger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetRecipes()
+        {
+            public static List<RecipeModel>() recipe =  new RecipeModel
+            {
+
+            }
+
+            return Ok(recipe);
+
+        }
+
+
+        //[HttpGet("id")]
+        //public ActionResult GetRecipesById()
+        //{
+
+        //}
+
+        [HttpPost]
+        public ActionResult CreateRecipes([FromBody] RecipeModel model)
+        {
+
+        }
+        //[HttpPut]
+        //public ActionResult CreateRecipes()
+        //{
+
+        //}
+        [HttpDelete("id")]
+        public async Task<IActionResult> RemoveRecipes(string id)
+        {
+            return NoContent();
+
+        }
+    }
+}
---
+++ RecipesManger/Models/RecipeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RecipesManger.Models
+{
+    public class RecipeModel
+    {
+        public int RecipeId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<string> Ingredients { get; set; }
+        public string Directions { get; set; }
+        public DateTime Modified { get; set; }
+
+    }
+}
